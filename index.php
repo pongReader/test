d7b4721a62bc9f7b5e8dfe8e121814c321adaf82
pongReader <rkdmf00002@gmail.com>
2017-03-14 17:10:33 +0900

    new_folder_in_dir

---
+++ index.php
@@ -0,0 +1,9 @@
+<script>
+
+var disX = (100)-(100);
+var disY = (100)-(100);
+var disZ = (100)-(95);
+
+distance = Math.sqrt(Math.abs(disX*disX) + Math.abs(disY*disY) + Math.abs(disZ*disZ));
+console.log(distance);
+</script>
